586e9d6ccc810b4d0422178b4b88d2706b05a753
one-data-cookie <kolacek.m@gmail.com>
2021-04-06 19:08:40 +0200

    improve indentation in marts models

--- dbt/models/marts/us_senate_party_seats.sql
+++ dbt/models/marts/us_senate_party_seats.sql
@@ -1,28 +1,31 @@
 with
 rnk as (
-  	select
-    		*,
-    		rank() over (
+    select
+        *,
+        rank() over (
             partition by
                 year,
-          			state,
-          			district
+                state,
+                district
             order by
                 votes_tally desc
         ) AS pos
-  	from {{ ref('stg_us_senate_election_results') }}
+    from
+        {{ ref('stg_us_senate_election_results') }}
 ),
 
 final as (
-  select
-  	year,
-  	party,
-  	count(*) as seats
-  from rnk
-  where pos = 1
-  group by
-    year,
-    party
+    select
+        year,
+        party,
+        count(*) as seats
+    from
+        rnk
+    where
+        pos = 1
+    group by
+        year,
+        party
 )
 
 select * from final
